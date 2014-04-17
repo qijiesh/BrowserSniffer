094eff27bc9598b8d42f4008ce54ff66cd307ea9
thankwsx <thankwsx@gmail.com>
2014-04-17 09:53:05 +0800

    补充一些IE系列的测试数据

---
+++ data.js
@@ -0,0 +1,9 @@
+var data = {
+    "ie11": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; Media Center PC 6.0; rv:11.0) like Gecko",
+    "ie10": "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)",
+    "ie9": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0",
+    "ie8": "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1;)",
+    "ie7": "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; el-GR)",
+    "ie6": "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)",
+    "ieother": "Mozilla/4.0 (compatible; MSIE 4.01; Windows NT 5.0)"
+}
