0fcd2cdbf0c34bc23e3065fb777055290d7f2fbf
thankwsx <thankwsx@gmail.com>
2014-04-17 16:09:48 +0800

    增加部分ua数据

--- data.js
+++ data.js
@@ -5,5 +5,22 @@ var data = {
     "ie8": "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1;)",
     "ie7": "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; el-GR)",
     "ie6": "Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)",
-    "ieother": "Mozilla/4.0 (compatible; MSIE 4.01; Windows NT 5.0)"
+    "ieother": "Mozilla/4.0 (compatible; MSIE 4.01; Windows NT 5.0)",
+
+    "qihu360": "Mozilla/5.0 (Window NT 6.1;WOW64) AppleWebkit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "chrome": "Mozilla/5.0 (Window NT 6.1;WOW64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36",
+    "safari": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1",
+    "sogou": "Mozilla/5.0 (Window NT 6.1;WOW64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36 SE 2.X MetaSr 1.0",
+    "qq": "Mozilla/5.0 (Window NT 6.1; WOW64; Trident/7.0;SLCC2; QQBrowser/7.6.21443.400; rv:11.0) like Gecko",
+    "maxthon": "Mozilla/5.0 (Window NT 6.1; WOW64;) AppleWebkit/537.1 (KHTML, like Gecko) Maxthon/4.1.3.5000 Chrome/26.0.1410.43 Safari/537.1",
+    "theworld": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; TheWorld)",
+    "liebao": "Mozilla/5.0 (Window NT 6.1;WOW64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36 LBBROWSER",
+    // "2345": "",
+    "firefox": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0",
+    // "taobao": "",
+    "baidu": "Mozilla/5.0 (Window NT 6.1;WOW64) AppleWebkit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 BIDUBrowser/2.x Safari/537.31",
+    "opera": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+    // "fengshu": ""
 }
+
+exports.data = data;
